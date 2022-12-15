c6fb50f4b305fbec8e331d0b2347823d34820acc
Chema Martinez <chema.martinez@wazuh.com>
2022-12-15 17:25:45 +0100

    Force failure in WPK upgrade

--- src/init/pkg_installer.sh
+++ src/init/pkg_installer.sh
@@ -196,7 +196,7 @@ chmod +x ./var/upgrade/install.sh
 ./var/upgrade/install.sh >> ./logs/upgrade.log 2>&1
 
 # Check installation result
-RESULT=$?
+RESULT=1
 
 echo "$(date +"%Y/%m/%d %H:%M:%S") - Installation result = ${RESULT}" >> ./logs/upgrade.log
 
