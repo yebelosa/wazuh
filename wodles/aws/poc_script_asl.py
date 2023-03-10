5b797767a161ce56855854b2eff37ec41456ed91
Mateo James <mateo.james@wazuh.com>
2023-03-10 16:56:47 -0300

    Working POC

--- wodles/aws/poc_script_asl.py
+++ wodles/aws/poc_script_asl.py
@@ -1,9 +1,41 @@
 #!/usr/bin/env python3
 import argparse
-import pandas
 import awswrangler as wr
-import sys, boto3, botocore, json
-import ast
+import sys, boto3, json
+import socket
+import os
+
+def find_wazuh_path() -> str:
+    """
+    Get the Wazuh installation path.
+
+    Returns
+    -------
+    str
+        Path where Wazuh is installed or empty string if there is no framework in the environment.
+    """
+    abs_path = os.path.abspath(os.path.dirname(__file__))
+    allparts = []
+    while 1:
+        parts = os.path.split(abs_path)
+        if parts[0] == abs_path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == abs_path:  # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            abs_path = parts[0]
+            allparts.insert(0, parts[1])
+
+    wazuh_path = ''
+    try:
+        for i in range(0, allparts.index('wodles')):
+            wazuh_path = os.path.join(wazuh_path, allparts[i])
+    except ValueError:
+        pass
+
+    return wazuh_path
 
 
 def get_sqs_client(access_key=None, secret_key=None, region=None, profile_name=None):
@@ -44,14 +76,17 @@ def get_parquet_location(sqs_client, sqs_queue):
     path = "s3://"+bucket_path+"/"+parquet_path
     return path
 
-def process_events_in_s3(s3_path):
 
-    list_of_jsons = (wr.s3.read_parquet(path=s3_path, path_suffix=".gz.parquet")).to_json(orient='records', lines=True, compression='gzip').split("\n")
-    #Read parquet from S3, turn it into JSON and send it to AnalysisD
-    for event_str in list_of_jsons:
-        event = json.loads(event_str)
-        #sendToAnaylisisD(event)
+def process_events_in_s3(s3_path):
+    asl_events_df = wr.s3.read_parquet(path=s3_path, path_suffix=".gz.parquet")
+    asl_events = [row.to_json() for row in asl_events_df.iloc]
+    wazuh_path = find_wazuh_path()
+    wazuh_queue = '{0}/queue/sockets/queue'.format(wazuh_path)
+
+    for event in asl_events:
+        send_msg(msg=event,msg_header="1:Wazuh-AWS:", wazuh_queue=wazuh_queue, dump_json=False)
         print("Event -->", event)
+
 
 def update_security_lake():
     sqs = get_sqs_client()
@@ -71,6 +106,34 @@ def get_script_arguments():
     parsed_args = parser.parse_args()
 
     return parsed_args
+
+
+def send_msg(msg, msg_header, wazuh_queue, dump_json=True):
+        """
+        Sends an AWS event to the Wazuh Queue
+
+        :param msg: JSON message to be sent.
+        :param dump_json: If json.dumps should be applied to the msg
+        """
+        try:
+            json_msg = json.dumps(msg, default=str)
+            s = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+            s.connect(wazuh_queue)
+            s.send("{header}{msg}".format(header=msg_header,
+                                          msg=json_msg if dump_json else msg).encode())
+            s.close()
+        except socket.error as e:
+            if e.errno == 111:
+                print("ERROR: Wazuh must be running.")
+                sys.exit(11)
+            elif e.errno == 90:
+                print("ERROR: Message too long to send to Wazuh.  Skipping message...")
+            else:
+                print("ERROR: Error sending message to wazuh: {}".format(e))
+                sys.exit(13)
+        except Exception as e:
+            print("ERROR: Error sending message to wazuh: {}".format(e))
+            sys.exit(13)
 
 
 def main():
