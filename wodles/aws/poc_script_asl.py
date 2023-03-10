0199044ada313cea3399f505a66d7caa2ad34fdb
Mateo James <mateo.james@wazuh.com>
2023-03-10 10:49:44 -0300

    Update decoder

--- wodles/aws/poc_script_asl.py
+++ wodles/aws/poc_script_asl.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import argparse
-
+import pandas
 import awswrangler as wr
 import sys, boto3, botocore, json
+import ast
 
 
 def get_sqs_client(access_key=None, secret_key=None, region=None, profile_name=None):
@@ -37,20 +38,26 @@ def get_parquet_location(sqs_client, sqs_queue):
     print(f'DEBUG: Message received is: {sqs_message}')
     body = sqs_message['Messages'][0]['Body']
     print(f'DEBUG: Body message is: {body}')
-    return body
+    message = json.loads(body)
+    parquet_path = message["detail"]["object"]["key"]
+    bucket_path = message["detail"]["bucket"]["name"]
+    path = "s3://"+bucket_path+"/"+parquet_path
+    return path
 
-# def process_events_in_s3(s3_path):
-#     #Read parquet from S3, turn it into JSON and send it to AnalysisD
-#     list_of_jsons = (wr.s3.read_parquet(path=s3_path,path_suffix=".gz.parquet")).to_json(orient = "records")
-#     for event in range(len(list_of_jsons)):
-#         sendToAnaylisisD(event)
-#
-#
-# def update_security_lake():
-#     sqs = get_sqs_client()
-#     paths_from_notifications = fetch_message_from_queue(sqs)
-#     for path in paths_from_notifications:
-#         process_events_in_s3(path)
+def process_events_in_s3(s3_path):
+
+    list_of_jsons = (wr.s3.read_parquet(path=s3_path, path_suffix=".gz.parquet")).to_json(orient='records', lines=True, compression='gzip').split("\n")
+    #Read parquet from S3, turn it into JSON and send it to AnalysisD
+    for event_str in list_of_jsons:
+        event = json.loads(event_str)
+        #sendToAnaylisisD(event)
+        print("Event -->", event)
+
+def update_security_lake():
+    sqs = get_sqs_client()
+    paths_from_notifications = fetch_message_from_queue(sqs)
+    for path in paths_from_notifications:
+        process_events_in_s3(path)
 
 
 def get_script_arguments():
@@ -72,7 +79,8 @@ def main():
     try:
         client = get_sqs_client()
         print(f'DEBUG: Retrieving notifications from: {options.sqs_queue}')
-        print(get_parquet_location(client, options.sqs_queue))
+        parquet_path = get_parquet_location(client, options.sqs_queue)
+        parquets = process_events_in_s3(parquet_path)
 
     except Exception as err:
         print("ERROR: {}".format(err))
